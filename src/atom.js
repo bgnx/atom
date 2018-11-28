ebcc727b46ead14c0155653776aa4ab5469ffb2d
bgnxmx <bgnxmx@gmail.com>
2018-11-28 16:54:47 +0300

    init

---
+++ src/atom.js
@@ -0,0 +1,71 @@
+const Stack = [];
+const NeedRecompute = [];
+
+export class Atom {
+  constructor(val, fn, active) {
+    this.value = val;
+    this.subs = new Set();
+
+    this.fn = fn;
+    this.deps = new Set();
+    this.oldDeps = null;
+    this.recompute.deep = 0;
+    this.active = active;
+    if (this.fn) this.recompute();
+  }
+  get() {
+    if (Stack.length > 0) {
+      Stack[Stack.length - 1].addDep(this);
+    }
+    return this.value;
+  }
+  set(val) {
+    if (this.value !== val) {
+      this.value = val;
+      this.subs.forEach(atom => NeedRecompute.indexOf(atom) === -1 && NeedRecompute.push(atom));
+      NeedRecompute.sort((left, right) => right.deep - left.deep);
+      if (NeedRecompute.length === this.subs.size) runComputation();
+    }
+  }
+  subscribe(fn) {
+    this.subs.add(fn);
+  }
+  unsubscribe(fn) {
+    this.subs.delete(fn);
+    if(!this.active && this.subs.size === 0) this.unsubscribeFromDeps()
+  }
+  addDep(atom) {
+    this.deps.add(atom);
+  }
+  recompute = () => {
+    this.oldDeps = this.deps;
+    this.deps = new Set();
+    Stack.push(this);
+    const newVal = this.fn();
+    Stack.pop();
+    for (const atom of this.oldDeps) {
+      if (!this.deps.has(atom)) {
+        atom.unsubscribe(this.recompute);
+      }
+    }
+    for (const atom of this.deps) {
+      if (!this.oldDeps.has(atom)) {
+        atom.subscribe(this.recompute);
+      }
+    }
+
+    let depsMaxDeep = 0;
+    this.deps.forEach(atom => depsMaxDeep = Math.max(atom.recompute.deep, depsMaxDeep));
+    this.recompute.deep = depsMaxDeep + 1;
+    this.set(newVal);
+  }
+  unsubscribeFromDeps() {
+    this.deps.forEach(dep => dep.unsubscribe(this.recompute));
+  }
+}
+
+function runComputation() {
+  while (NeedRecompute.length) {
+    NeedRecompute.pop()();
+  }
+}
